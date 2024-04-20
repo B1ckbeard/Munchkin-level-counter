1ace4cf39c402ff62ce3fe66b69e44989b9d0158
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2024-04-20 19:36:44 +0300

    some changes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -25,46 +25,50 @@ const Counter = ({ id, name, lvl, itemsLvl, onRemove }) => {
   const itemsPowerInc = () => setItemsPowerCount(itemsPowerCount + clickValue);
   const itemsPowerDec = () => itemsPowerCount > 0 ? setItemsPowerCount(itemsPowerCount - clickValue) : null;
 
-  useEffect (() => {
+  useEffect(() => {
     dispatch(actions.updateCounter({
-      id:id, changes: { lvl: levelCount, itemsLvl: itemsPowerCount }
+      id: id, changes: { lvl: levelCount, itemsLvl: itemsPowerCount }
     }));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [levelCount, itemsPowerCount]);
 
-  useEffect (()=> {
+  useEffect(() => {
     console.log(counters);
     window.localStorage.setItem('counters', JSON.stringify(counters));
   }, [counters]);
 
   return (
-    <div className='container w-50 p-2 my-2 text-center'>
-      <div className="row d-flex">
-        <div className="col-6 d-flex justify-content-start align-items-center fs-2 fw-bold">
-          {isRemoveable && (
-            <div className="me-2 d-flex align-items-center">
-              <XCircleFill onClick={onRemove} size={36} style={{ color: 'red' }} />
+    <div className="container w-100 d-flex justify-content-center mb-3">
+      <div className="counter__wrapper">
+        <div className='counter__container'>
+          <div className="row d-flex">
+            <div className="col-6 d-flex justify-content-start align-items-center fs-2 fw-bold">
+              {name}
             </div>
-          )}
-          {name}
+            <div className="col-6 d-flex justify-content-end align-items-center">
+              <img src={levelUpIcon} alt="levelUpIcon" style={{ width: '30px' }} />
+              <div className="fs-3 me-1" style={{ width: '2rem' }}>{levelCount}</div>
+              <Button onClick={levelInc} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1 me-1' style={{ width: '35px', height: '35px' }}>+</Button>
+              <Button onClick={levelDec} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1' style={{ width: '35px', height: '35px' }}>-</Button>
+            </div>
+          </div>
+          <div className="row d-flex">
+            <div className="col-6 d-flex justify-content-start align-items-center fs-2">
+              <div className="fs-3">Сила: <span className='fw-bold'>{powerCount}</span></div>
+            </div>
+            <div className="col-6 d-flex justify-content-end align-items-center">
+              <img src={swordIcon} alt="swordIcon" style={{ width: '30px' }} />
+              <div className="fs-3 me-1" style={{ width: '2rem' }}>{itemsPowerCount}</div>
+              <Button onClick={itemsPowerInc} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1 me-1' style={{ width: '35px', height: '35px' }}>+</Button>
+              <Button onClick={itemsPowerDec} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1' style={{ width: '35px', height: '35px' }}>-</Button>
+            </div>
+          </div>
         </div>
-        <div className="col-6 d-flex justify-content-end align-items-center gap-1">
-          <img src={levelUpIcon} alt="levelUpIcon" style={{ width: '30px' }} />
-          <div className="fs-3" style={{ width: '2rem' }}>{levelCount}</div>
-          <Button onClick={levelInc} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1' style={{ width: '35px', height: '35px' }}>+</Button>
-          <Button onClick={levelDec} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1' style={{ width: '35px', height: '35px' }}>-</Button>
-        </div>
-      </div>
-      <div className="row d-flex">
-        <div className="col-6 d-flex justify-content-start align-items-center fs-2">
-          <div className="fs-2">Сила: <span className='fw-bold'>{powerCount}</span></div>
-        </div>
-        <div className="col-6 d-flex justify-content-end align-items-center gap-1">
-          <img src={swordIcon} alt="swordIcon" style={{ width: '30px' }} />
-          <div className="fs-3" style={{ width: '2rem' }}>{itemsPowerCount}</div>
-          <Button onClick={itemsPowerInc} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1' style={{ width: '35px', height: '35px' }}>+</Button>
-          <Button onClick={itemsPowerDec} variant="outline-dark" className='rounded-5 d-flex align-items-center justify-content-center fs-1' style={{ width: '35px', height: '35px' }}>-</Button>
-        </div>
+        {isRemoveable && (
+          <div className="delete">
+            <XCircleFill onClick={onRemove} size={28} style={{ color: 'red' }} />
+          </div>
+        )}
       </div>
     </div>
   )
